7f28adea16c8abfbd9e6805e29f9c24821fc0a13
EchoHall <lehoangquan.lhq@gmail.com>
2024-09-23 10:23:48 -0600

    update

--- interactive-screen/sketch.js
+++ interactive-screen/sketch.js
@@ -2,25 +2,26 @@
 
 
 
-let width = 750;
-let height = 600;
+// let width = 750;
+// let height = 600;
 
 let radius = 80;
 let og_spot_circlex;
-let og_spot_circley = height - radius;
+let og_spot_circley;
 
 let falling_rate = 5;
-let ball_y = radius/2;
-let ball_x = width/2;
+let ball_y;
+let ball_x;
 let falling_length = 0;
 
 function setup() {
-  createCanvas(width, height);
+  createCanvas(windowWidth, windowHeight);
 
 }
 
 function draw() {
   background(220);
+
   small_ball();
   
   main_catcher();
@@ -32,11 +33,17 @@ function main_catcher(){
     //main catcher
   fill("brown");
   og_spot_circlex = mouseX;
+  og_spot_circley = height - radius - 3;
+
   arc(og_spot_circlex, og_spot_circley,radius*2,radius*2,0, PI, CHORD);
+  circle(mouseX - radius, height, radius/2)
 }
 
 function fall_small_ball(){
     //movement of small ball
+  ball_x = radius/2
+  ball_x = width/2
+
   if (ball_y > 0 ) {
     ball_y = ball_y + falling_rate;
   }
@@ -53,10 +60,3 @@ function small_ball(){
   circle(ball_x, 0 + ball_y, radius/2);
 }
 
-function draw_background(){
-  for(let y = 0; y < height; y++){
-    for(let x = 0; x < width; x++){
-      circle(x, y, 3)
-    }
-  }
-}
