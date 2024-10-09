47a7d0ba72deab9c117969492b1dec56621e08aa
EchoHall <lehoangquan.lhq@gmail.com>
2024-10-09 11:02:50 -0600

    make the ball

--- array/sketch.js
+++ array/sketch.js
@@ -5,36 +5,45 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cellSize;
+let cellSize = 80;
 
-
+let boardx;
+let boardy;
 let purpleBall;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  if (width > height) {
-    cellSize = height/10;
-  }
-  else {
-    cellSize = width/10;
-  }
+ 
+  boardy = height/8;
+  boardx = width/4;
+
+
+  purpleBall = {
+    x: boardx + cellSize/2,
+    y: boardy + cellSize/2,
+    size: 65,
+    color: "purple",
+  };
+
 }
 
 function draw() {
   background(220);
 
   makeGrid();
+
+  makeOrb();
 }
 
 function makeGrid(){
   for (let y = 0; y < 8; y++) {
     for (let x = 0; x < 8; x++) {
-      square(width/4 + x * cellSize, height/8+ y * cellSize, cellSize);
+      square(boardx + x * cellSize, boardy + y * cellSize, cellSize);
     }
   }
 }
 
 function makeOrb(){
-  
+  circle(purpleBall.x, purpleBall.y, purpleBall.size);
+  fill(purpleBall.color);
 }
