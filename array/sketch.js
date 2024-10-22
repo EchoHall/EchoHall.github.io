08c71e4bfdf25f02c50b9daf3e5dcded49556d5c
EchoHall <lehoangquan.lhq@gmail.com>
2024-10-22 01:38:31 -0400

    done?

--- array/sketch.js
+++ array/sketch.js
@@ -2,12 +2,9 @@
 // Quan Le
 // October 8th
 //
-// SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Generate random color orb. When the mouse is clicked, there will be some numbers appear.
 
 let cellSize = 80;
-let theOrbs = [];
 
 const movementAmount = [0, 1, 2, 3, 4, 5, 6, 7];
 
@@ -15,6 +12,8 @@ let newOrbs;
 
 let boardx;
 let boardy;
+
+let someOrb = [];
 
 
 function setup() {
@@ -25,7 +24,7 @@ function setup() {
 
   newOrbs = {
     size: 65,
-    color: ["red","green", "orange", "blue", "yellow", "black", "white"],
+    color:  ["red","green", "orange", "blue", "yellow", "black", "white"],
   };
   //loading 
 
@@ -48,55 +47,36 @@ function makeGrid(){
   }
 }
 
-// function locateOrbs() {
-//   for(let orb = 0; orb < 64; orb++) {
-//     theRandomNumberX = random(movementAmount);
-//     theRandomNumberY = random(movementAmount);
-//     randomColor = random(color);
-
-//     let newOrbs = {
-//       x:  theRandomNumberX,
-//       y:  theRandomNumberY,
-//       size: 65,
-//       color: randomColor,
-//     };
-
-//     let isNewLocation = false; 
-//     let isNewColor = false;
-
-//     while (!isNewLocation) {
-//       newListX = structuredClone(movementAmount);
-//       newListY = structuredClone(movementAmount);
-
-//       for (let theOrb of theOrbs) {
-//         if (theOrb.x === newOrbs.x) {
-//           let leftoverY = newListY.splice(theOrb.y);
-//           newOrbs.y = random(newListY);
-//         }
-
-//         if (theOrb.y === newOrbs.y) {
-//           let leftoverX = newListX.splice(theOrb.x);
-//           newOrbs.x = random(newListX);
-//         }
-
-//         newListX.push(leftoverX);
-//         newListY.push(leftoverY);
-
-//       }
-//       isNewLocation = true;
-//     }
-//     theOrbs.push(newOrbs);
-//     makeOrb();
-//   }
-
-// }
 
 
 function makeOrb(){
   for(let y = 0; y < 8; y++){
     for (let x = 0; x < 8; x++){
-      fill(random);
+      fill(random(newOrbs.color));
       circle(cellSize/2 + width/4 + cellSize*x, cellSize/2 + height/8 + cellSize*y, newOrbs.size);
+
+      if (x === y){
+        someOrb.push(x/10 + y/10);
+      }
+
+      else{
+        someOrb.push(x/y);
+      }
     }
   }
 }
+
+function mousePressed(){
+  for(let y = 0; y < 8; y++){
+    for (let x = 0; x < 8; x++){
+      if (someOrb[y+x] < 0){
+        text(someOrb[y+x], cellSize/2 + width/4 + cellSize*x, cellSize/2 + height/8 + cellSize*y);
+      }
+
+      else{
+        text(someOrb[y+x], cellSize/2 + width/4 + cellSize*x, cellSize/2 + height/8 + cellSize*y);
+      }
+    }
+  }
+}
+
